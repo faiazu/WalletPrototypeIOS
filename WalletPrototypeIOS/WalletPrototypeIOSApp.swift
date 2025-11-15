361871e8502733d3f5255cae8b62913dd8e5da1f
Faiaz Umaer <faiazumaer@gmail.com>
2025-11-15 17:03:29 -0500

    Initial Commit

---
+++ WalletPrototypeIOS/WalletPrototypeIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletPrototypeIOSApp.swift
+//  WalletPrototypeIOS
+//
+//  Created by Faiaz on 2025-11-15.
+//
+
+import SwiftUI
+
+@main
+struct WalletPrototypeIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
